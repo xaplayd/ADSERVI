6c26ea290848a971e19926238da41ab870bf0fe3
xaplayd <douglas_comelli_balbinot@hotmail.com>
2022-11-13 14:00:22 -0300

    Created updateUsersList on User class (funtion ejection)

--- src/models/Usuario.java
+++ src/models/Usuario.java
@@ -1,6 +1,13 @@
 package models;
 
-public class Usuarios {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
 	
 	private Integer codigo;
 	private String nome;
@@ -11,20 +18,14 @@ public class Usuarios {
 	private String emailGerencia;
 	private String setor;
 	private Integer situacao;
+	private List<Usuario> listaDeUsuarios = new ArrayList<Usuario>();
+	private String filePath = "src\\dados\\tblUsuarios.csv";
+
 	
-	public Usuarios() {
+	public Usuario() {
 	}
 	
-	//construtor de usuario para login
-	public Usuarios(String login, String senha, Integer permissoes, Integer situacao) {
-		this.login = login;
-		this.senha = senha;
-		this.permissoes = permissoes;
-		this.situacao = situacao;
-	}
-	
-	//construtor de usuario completo
-	public Usuarios(Integer codigo, String nome, String login, String senha, Integer permissoes, String email,
+	public Usuario(Integer codigo, String nome, String login, String senha, Integer permissoes, String email,
 			String emailGerencia, String setor, Integer situacao) {
 		this.codigo = codigo;
 		this.nome = nome;
@@ -36,60 +37,87 @@ public class Usuarios {
 		this.setor = setor;
 		this.situacao = situacao;
 	}
+	
 	public Integer getCodigo() {
 		return codigo;
 	}
+	
 	public void setCodigo(Integer codigo) {
 		this.codigo = codigo;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getLogin() {
 		return login;
 	}
+	
 	public void setLogin(String login) {
 		this.login = login;
 	}
+	
 	public String getSenha() {
 		return senha;
 	}
+	
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+	
 	public Integer getPermissoes() {
 		return permissoes;
 	}
+	
 	public void setPermissoes(Integer permissoes) {
 		this.permissoes = permissoes;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getEmailGerencia() {
 		return emailGerencia;
 	}
+	
 	public void setEmailGerencia(String emailGerencia) {
 		this.emailGerencia = emailGerencia;
 	}
+	
 	public String getSetor() {
 		return setor;
 	}
+	
 	public void setSetor(String setor) {
 		this.setor = setor;
 	}
+	
 	public Integer getSituacao() {
 		return situacao;
 	}
+	
 	public void setSituacao(Integer situacao) {
 		this.situacao = situacao;
 	}
+	
+	public String getFilePath() {
+		return filePath;
+	}
+	
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	
 	@Override
 	public String toString() {
 		return "Usuario [codigo=" + codigo + ", nome=" + nome + ", login=" + login + ", senha=" + senha
@@ -97,8 +125,53 @@ public class Usuarios {
 				+ setor + "]";
 	}
 	
+	public boolean validaUsuarioLogin(String login, String senha) {
+		int validaLogin = 0;
+		int validaSenha = 0;
+		int validaSituacao = 0;
+		
+		if(getLogin() == login) {
+			validaLogin = 1;
+		}
+		if(getSenha() == senha) {
+			validaSenha = 1;
+		}
+		if(getSituacao() == 1) {
+			validaSituacao = 1;
+		}
+		if (validaLogin == 1 && validaSenha == 1 && validaSituacao == 1) {
+			return true;
+		}
+		
+		return false;
+		
+	}
 	
-	
+	public void updateListaUsuarios() {
+		
+		File arquivoUsuarios = new File(filePath);
+
+		try (BufferedReader tblUsuarios = new BufferedReader(new FileReader(arquivoUsuarios))) {
+			String usuario = tblUsuarios.readLine();
+			while (usuario != null) {
+				String[] fields = usuario.split(";");
+				Integer codigo = Integer.parseInt(fields[0]);
+				String nome = fields[1];
+				String login = fields[2];
+				String senha = fields[3];
+				Integer permissoes = Integer.parseInt(fields[4]);
+				String email = fields[5];
+				String emailGerencia = fields[6];
+				Integer situacao = Integer.parseInt(fields[7]);
+				
+				listaDeUsuarios.add(new Usuario(codigo, nome, login, senha, permissoes, email, emailGerencia, setor, situacao));
+				usuario = tblUsuarios.readLine();
+			}
+		} catch (IOException e) {
+			e.getMessage();
+		}
+		
+	}
 	
 
 }
